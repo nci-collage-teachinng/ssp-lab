30861c20e96a92fd242ae50d5ad22a0839ec5ec5
Sumit <8011717+trsumit@users.noreply.github.com>
2022-11-06 22:15:41 +0000

    Create hadoop-copy.sh

---
+++ hadoop-copy.sh
@@ -0,0 +1,8 @@
+wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.4/hadoop-3.3.4.tar.gz
+tar -xvf hadoop-3.3.4.tar.gz
+sudo mv hadoop-3.3.4 /opt/hadoop
+sudo chmod 777 /opt/hadoop/
+sudo mkdir -p /usr/local/hadoop/hdfs/namenode
+sudo chmod 777 /usr/local/hadoop/hdfs/namenode
+sudo mkdir -p /usr/local/hadoop/hdfs/datanode
+sudo chmod 777 /usr/local/hadoop/hdfs/datanode
